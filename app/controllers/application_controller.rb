345690461982b855e772602fe6b34228aa813623
hudabz <huda_bz@hotmail.com>
2019-06-19 15:09:31 +0300

    version 2 after updating the heroku problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_artist! 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-      def configure_permitted_parameters
-         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :age, :country])
-      end
+    protected 
+      def configure_permitted_parameters 
+         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :age, :country]) 
+      end 
     
 end
